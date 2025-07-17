07bf356fae115827340a3cf34873d6326752872b
Smit275-z <w0849357@myscc.ca>
2025-07-16 22:11:19 -0300

    Background color added

--- Assignment#7.swiftpm/ContentView.swift
+++ Assignment#7.swiftpm/ContentView.swift
@@ -1,12 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var email = ""
+    @State private var phone = ""
+    @State private var numberOfPeople = 1
+    @State private var selectedMeal = "Breakfast"
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+
+    let customBackground = Color(red: 0.95, green: 0.9, blue: 0.85) // Custom color
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView { //  Layout for both orientations
+              
+            }
+            .background(customBackground.ignoresSafeArea())
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Reservation"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
